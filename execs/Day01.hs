66e820c610e3bedd104ef6cc16e383dd3d54cd03
Paolo Martini <mrtnpaolo@protonmail.com>
2025-12-01 20:52:19 +0100

    🏎️ day-01: different approach, count crossings
    Inspired by glguy's version here:
    
    <https://github.com/glguy/advent/blob/main/solutions/src/2025/01.hs>
    
    I first ended up with the following:
    
    ```
    click (_,n) k
      | k > 0                                    = (n+k) `divMod` 100
      | k < 0, (q,r) <- (sym n - k) `divMod` 100 = (q,sym r)
    
    sym x = negate x `mod` 100
    ```
    
    Then I decided to try to understand exactly how `div`'s behavior clashed
    with the idea of counting 0-crossings which lead to fixing its result in
    the two particular ways explained in the comments.
    
    Given that AoC is an exercise in aesthetics more than anything for me,
    here's two aspects that I find very satisfactory:
     * `part1` and `part2` ended up extremely compact, which is lovely
     * `unzip` shows how half of the computed accumulators serve one
       purpose, half the other.

--- execs/Day01.hs
+++ execs/Day01.hs
@@ -3,17 +3,22 @@ module Main (main) where
 import Advent (getInputLines, count)
 
 main =
-  do inp <- scanl clicks [50] <$> getInputLines parse 1
-     print (part1 inp)
-     print (part2 inp)
+  do (ps,zs) <- unzip . scanl click (0,50) <$> getInputLines parse 1
+     print (part1 zs)
+     print (part2 ps)
   where
     parse ('L':n) = negate (read @Int n)
     parse ('R':n) =        (read @Int n)
 
-clicks (last -> n) k = take (abs k) . tail . iterate click $ n
-  where
-    click = (`mod` 100) . (+ (signum k))
+    click (_,n) k = (crossings, r)
+      where
+        (q,r) = (n + k) `divMod` 100
+        crossings
+          | k > 0     = abs q
+          | otherwise = abs q   -- two problems arise when counting going left:
+            + fromEnum (r == 0) -- land on zero, not counted by divMod, add one
+            - fromEnum (n == 0) -- start from zero, counted extra, subtract one
 
-part1 = count (0 ==) . map last
+part1 = count (0 ==)
 
-part2 = count (0 ==) . concat
+part2 = sum
