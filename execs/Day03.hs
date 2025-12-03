6e02a05099c53646add763ca3b79986d56d7d639
Paolo Martini <mrtnpaolo@protonmail.com>
2025-12-03 14:06:13 +0100

    🏎️ day-03: way faster by directly selecting the best digits
    I particularly love that the Ord monoid approach turned out to be the
    fastest in my tests!
    
    It turned out to be even faster than rewriting `maximumBy` to thread
    over the first maximum instead of the last:
    
      maximumBy cmp = foldl1' maxBy
        where
          maxBy x y = case cmp x y of
                        GT -> x
    		    EQ -> x
    		    LT -> y
    
    I find this surprising. It might be the case that GHC's one which does:
    
      case cmp x y of
        GT -> x
        _  -> y
    
    has more chances to be fused with the rest of the code?
    
      <https://github.com/ghc/ghc/blob/fc1d7f7966f56bbe5efaf2796c430dbe526b834b/libraries/ghc-internal/src/GHC/Internal/Data/OldList.hs#L946>
    
    I don't see explicit pragmas about rewriting or fusing but I don't know
    what actually *can* be up with this.
    
    Let's just say that `comparing head <> comparing length` is beautiful
    and I am very happy it also gives the code a performance boost ☺️

--- execs/Day03.hs
+++ execs/Day03.hs
@@ -1,43 +1,23 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent    (getInputLines)
+import Data.Ord  (comparing)
+import Data.List (maximumBy, tails)
+import Data.Char (digitToInt, intToDigit)
 
 main =
-  do inp <- getInputLines parse 3
+  do inp <- getInputLines (map digitToInt) 3
      print (part1 inp)
      print (part2 inp)
+
+part1 = score . map (pick 2)
+
+part2 = score . map (pick 12)
+
+pick 0 _  = []
+pick n xs = x : pick (n-1) best
   where
-    parse = map digitToInt
+    viable   = take (length xs - n + 1) (tails xs)
+    (x:best) = maximumBy (comparing head <> comparing length) viable
 
-part1 = sum . map maxjoltage
-
-maxjoltage xs = maximum [ a*10+b | (a,b) <- pairs xs ]
-
-part2 = sum . map (read @Int . map intToDigit . twelve)
-
-twelve xs = head [ reverse ds | (ds,12,_,_) <- bfsOn repr next [start] ]
-  where
-    start = ([],0,xs,length xs)
-
-    repr (ds,_,rs,_) = (ds,rs)
-
-    next (ds,n,rs,r) -- (picked, length picked, remaining, length remaining)
-      | n < 12
-      , rss <- takeWhile ((>= (12-n)) . length) . L.tails $ rs
-      , ((_,x:xs):_) <- L.sortOn fst [ (-x,xs) | xs@(x:_) <- rss ]
-      = [ (x:ds,n+1,xs,r-1) ]
-      | otherwise = []
+score = sum . map (read @Int . map intToDigit)
