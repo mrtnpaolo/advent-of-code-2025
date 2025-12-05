8f83e13ab4da5a503329136dfb7160ecca15fa20
Paolo Martini <mrtnpaolo@protonmail.com>
2025-12-05 02:36:07 +0100

    🏎️ day-04: linear solution
    The problem is equivalent to the k-core problem described here:
    
        <https://en.wikipedia.org/wiki/Degeneracy_(graph_theory)>
    
    Which has a O(V+E) linear solution that I implemented in this commit.
    
    We are using a list as a stack because we prepend at most 8 elements,
    which means we search depth first and CS.notMember skips re-checks.
    
    The addition of Coord.Set was key to achieve a program runtime that is
    comparable to that of an "hello world" Haskell program.
    
    So far all the programs run in the 16-18ms runtime on my machine, this
    one being the fastest of the bunch! Very pleased.
    
    I am also liking how every part 1 so far has been super straightforward
    and every part 2 had some interesting nuance to it that pays thinking
    about. It might be the result of this compressed 25->12 days AoC.

--- execs/Day04.hs
+++ execs/Day04.hs
@@ -1,22 +1,30 @@
 module Main (main) where
 
-import Advent             (getInputArray, neighbors)
-import Data.List          (unfoldr)
-import Data.Array.Unboxed (assocs, (!), inRange, bounds, (//))
-import Control.Monad      (guard)
+import Advent           (getInputMap)
+import Advent.Coord.Set qualified as CS
+import Data.Map         qualified as M
+
+k = 4
 
 main =
-  do inp <- getInputArray 4
-     print (part1 inp)
-     print (part2 inp)
+  do inp <- getInputMap 4
+     let s = CS.fromSet . M.keysSet . M.filter ('@'==) $ inp
+         q = [i | i <- CS.toList s, length (live s i) < k]
+     print (part1 q)
+     print (part2 s q)
 
-part1 = length . accessible
+part1 = length
 
-accessible a =
-  [ c | (c,'@') <- assocs a
-      , length [ n | n <- neighbors c, inRange (bounds a) n, a!n == '@' ] < 4 ]
+part2 s q = CS.size s - CS.size (kcore s q)
 
-part2 = sum . unfoldr \a ->
-  do let cs = accessible a
-     guard (not (null cs))
-     pure (length cs, a // [ (c,'.') | c <- cs ])
+live s c = [n | n <- CS.neighbors s c, CS.member n s]
+
+-- k-core in linear time https://en.wikipedia.org/wiki/Degeneracy_(graph_theory)
+
+kcore s []    = s
+kcore s (c:q)
+  | CS.notMember c s = kcore s q
+  | length ns >= k   = kcore s q
+  | otherwise        = kcore (CS.delete c s) (ns ++ q)
+  where
+    ns = live s c
