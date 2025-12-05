08f761828508b1531a1fbbb5a86c7a2ce3a7ba03
Paolo Martini <mrtnpaolo@protonmail.com>
2025-12-05 14:59:26 +0100

    ✍️ day-05: keep track of a cursor through sorted ranges, no merges

--- execs/Day05.hs
+++ execs/Day05.hs
@@ -2,11 +2,11 @@ module Main (main) where
 
 import Advent          (getInput, count)
 import Data.Ix         (inRange)
-import Data.List       (foldl', sort, findIndex)
+import Data.List       (foldl', sort)
 import Data.List.Split (splitOn)
 
 main =
-  do (foldl' combine [] . sort -> rs,xs) <- getInput (parse . splitOn "\n\n") 5
+  do (rs,xs) <- getInput (parse . splitOn "\n\n") 5
      print (part1 rs xs)
      print (part2 rs)
   where
@@ -16,14 +16,9 @@ main =
 
 part1 rs = count (\x -> any (`inRange` x) rs)
 
-part2 rs = sum [ h-l+1 | (l,h) <- rs ]
-
-combine rs (l,h) =
-  case ( rs ? l , rs ? h ) of
-    ( Just i  , Just j  ) | (xs,(a,_): _) <- splitAt i rs
-                          , ( _,(_,b):ys) <- splitAt j rs -> (xs ++ (a,b) : ys)
-    ( Just i  , Nothing ) | (xs,(a,_):ys) <- splitAt i rs -> (xs ++ (a,h) : ys)
-    ( Nothing , Just j  ) | (xs,(_,b):ys) <- splitAt j rs -> (xs ++ (l,b) : ys)
-    ( Nothing , Nothing )                                 -> (      (l,h) : rs)
-
-rs ? x = findIndex (`inRange` x) rs
+part2 = fst . foldl' go (0,0) . sort
+  where
+    go (n,x) (l,h)
+      | x >  h    = (n          ,x  )
+      | x <= l    = (n + (h-l+1),h+1)
+      | otherwise = (n + (h-x+1),h+1)
