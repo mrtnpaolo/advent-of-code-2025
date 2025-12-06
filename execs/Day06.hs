9766b69c586f3916606bd421fcd501226fe553d3
Paolo Martini <mrtnpaolo@protonmail.com>
2025-12-06 22:50:31 +0100

    🧼 day-06: clean up

--- execs/Day06.hs
+++ execs/Day06.hs
@@ -1,38 +1,17 @@
 module Main (main) where
 
-import Advent
-import Numeric
-import Data.Ix
-import Data.Ord
-import Data.Char
-import Data.Maybe
-import Data.Either
-import Data.List          qualified as L
-import Data.List.Split    qualified as L
-import Data.Set           qualified as S
-import Data.Map.Strict    qualified as M
-import Data.IntSet        qualified as IS
-import Data.IntMap.Strict qualified as IM
-import Data.Array.Unboxed qualified as A
-import Debug.Trace
+import Advent          (getInput)
+import Data.List       (transpose)
+import Data.List.Split (splitWhen)
 
 main =
-  do inp1 <- getInput (map parse1 . pre1) 6
-     print (part1 inp1)
-     inp2 <- getInput (map parse2 . pre2) 6
-     print (part2 inp2)
-  where
-    pre1            = L.transpose . reverse . map words . lines
-    parse1 ([f]:xs) = (fn f,map read xs)
+  do inp <- getInput (splitWhen (all (' ' ==)) . transpose . lines) 6
+     print (part1 inp)
+     print (part2 inp)
 
-    pre2              = map (reverse . L.transpose)
-                      . L.splitWhen (all (' '==)) . L.transpose . lines
-    parse2 ((f:_):xs) = (fn f,map read . L.transpose . reverse $ xs)
+part1 = sum . map ((\xs    -> w (head (last xs)) (init xs)) . transpose)
 
-    fn '*' = product
-    fn '+' = sum
+part2 = sum . map (\(x:xs) -> w (last x) (init x : xs))
 
-part1 = sum . map (uncurry ($))
-
-part2 = part1
-
+w '+' =     sum . map read
+w '*' = product . map read
