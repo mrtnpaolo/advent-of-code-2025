de011ce6fc3d08b3021bc04f1a825461df7026dd
Paolo Martini <mrtnpaolo@protonmail.com>
2025-12-08 23:26:21 +0100

    🏎️ day-08: improve performance by sorting the points only by x
    Then work through the pairs in batches that we can filter through
    efficiently through the soorted coordinate.
    
      Time:      55.1 ms ±   0.6 ms    [User: 42.2 ms, System: 5.6 ms]
      Range:     54.0 ms …  55.7 ms    10 runs
    
    There's a little bit more work I can avoid doing: the O(n) IM.map.
    
    Also fixed a little bug where go was pooping out cs' at each step
    even when there was no reason to return the modified version.
    
    It wasn't a performance concern just logically wrong.

--- execs/Day08.hs
+++ execs/Day08.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Advent (getInputLines,pairs)
+import Advent (getInputLines)
 import Data.Ord (Down(..))
 import Data.List          qualified as L
 import Data.IntMap.Strict qualified as IM
@@ -18,27 +18,28 @@ part1 ((!! (1000 - 1)) -> (_,cs))
 
 part2 (last -> (((xp,_,_),(xq,_,_)),_)) = xp * xq
 
-connect points = go (IM.size m) IM.empty ps
+connect points = go (length sorted) cs0 pairs
   where
+    sorted = L.sortOn (\(x,_,_) -> x) points
+
     hash (x,y,z) = 100*x + 10*y + z
-    m = IM.fromList [ (hash p,p) | p <- points ]
-    l = (m IM.!)
+    cs0 = IM.fromList [(hash p,hash p) | p <- sorted]
 
     dist (x1,y1,z1) (x2,y2,z2) = dx*dx + dy*dy + dz*dz
-      where
-        (dx,dy,dz) = (x1-x2,y1-y2,z1-z2)
+      where (dx,dy,dz) = (x1-x2,y1-y2,z1-z2)
 
-    ps = [ (hash p,hash q) | (p,q) <- L.sortOn (uncurry dist) (pairs points) ]
+    pairs        = concat [ inside lo hi | lo <- 0:bands | hi <- bands ]
+    bands        = [20000,22500..]
+    inside lo hi = L.sortOn (uncurry dist)
+      [ (p,q) | p@(px,_,_) : qs <- L.tails sorted
+              , q <- takeWhile (\(qx,_,_) -> qx < px + hi) qs
+              , let d = dist p q, lo*lo <= d, d < hi*hi ]
 
     go 1 _ _ = []
-    go n cs ((p,q):ps)
-      | p'  <- find cs p
-      , q'  <- find cs q
-      , cs' <- IM.insert p' q' cs
-      = ((l p,l q),cs') : if p' == q' then go n cs ps else go (n-1) cs' ps
-
-    find cs p
-      | p == p'   = p
-      | otherwise = find cs p'
+    go n cs ((p,q):rs)
+      | sp == sq  = ((p,q),cs ) : go n     cs  rs
+      | otherwise = ((p,q),cs') : go (n-1) cs' rs
       where
-        p' = IM.findWithDefault p p cs
+        sp  = cs IM.! hash p
+        sq  = cs IM.! hash q
+        cs' = IM.map (\v -> if v == sp then sq else v) cs
